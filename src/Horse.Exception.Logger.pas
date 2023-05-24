48e2817ca8834c371cb500ed4939c0ea2059a315
arvanus <lucasschatz@gmail.com>
2023-05-24 12:40:41 -0300

    Update Horse.Exception.Logger.pas
    Fix AsJSON to ToJSON

--- src/Horse.Exception.Logger.pas
+++ src/Horse.Exception.Logger.pas
@@ -119,7 +119,7 @@ var
     LLog := LLog.Replace('${response_title}', THorseExceptionLogger.ValidateValue(AResponse.RawWebResponse.Title));
     LLog := LLog.Replace('${response_content_version}', THorseExceptionLogger.ValidateValue(AResponse.RawWebResponse.ContentVersion));
     {$ENDIF}
-    LLog := LLog.Replace('${exception}', THorseExceptionLogger.ValidateValue(LJSON.AsJSON));
+    LLog := LLog.Replace('${exception}', THorseExceptionLogger.ValidateValue(LJSON.ToJSON));
   end;
 begin
   LBeforeDateTime := Now();
